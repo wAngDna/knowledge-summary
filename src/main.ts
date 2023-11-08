157035b2c72f5c34538ece2168b0f60832610bde
Dion <Dion@wtex.us>
2023-11-08 14:45:20 +0800

    docs: 📝  update docs

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { createApp } from 'vue'
 import App from './App'
 import ViewUIPlus from 'view-ui-plus'
 import VMdPreview from '@kangc/v-md-editor/lib/preview'
-import '@kangc/v-md-editor/lib/style/preview.css'
 import githubTheme from '@kangc/v-md-editor/lib/theme/github.js'
+import Prism from 'prismjs'
 import hljs from 'highlight.js'
 import 'highlight.js/styles/atom-one-dark-reasonable.css'
 VMdPreview.use(githubTheme, {
+  Prism,
   Hljs: hljs
 })
 import 'view-ui-plus/dist/styles/viewuiplus.css'
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,7 @@
 /* eslint-env node */
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier', 'stylelint-config-html/vue']
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier', 'stylelint-config-html/vue'],
+  rules: {
+    'selector-class-pattern': null
+  }
 }
