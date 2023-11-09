b4053264fb275b791df06d0e728fe0e4e8a7e0ca
Dion <Dion@wtex.us>
2023-11-09 16:45:25 +0800

    test: 🚨  test sync github

--- scripts/pre-commit.ts
+++ scripts/pre-commit.ts
@@ -8,22 +8,24 @@ await import('./check').catch((out) => {
   throw new Error('代码质量评估失败, 请检查代码.')
 })
 
-console.log('printf "检测通过, run build now...\n')
+console.log('检测通过, run build now...\n')
 
 await import('./build').catch((out) => {
   throw new Error('代码打包失败, 请修复.')
 })
 
-console.log('printf "build success, run deploy now...\n')
+console.log('build success, run deploy now...\n')
 
 await import('./deploy').catch((out) => {
   throw new Error('Git Pages上传失败, 请排查原因.')
 })
 
-console.log('printf "deploy success,  Sync GitHub now...\n')
+console.log('deploy success,  Sync GitHub now...\n')
 
 await import('./syncgithub').catch((out) => {
-  throw new Error('GitHub Push, 请排查原因.')
+  throw new Error('GitHub Push失败, 请排查原因.')
 })
+
+console.log('github push success,  创建commit...\n')
 
 await $`git add .`
--- scripts/syncgithub.ts
+++ scripts/syncgithub.ts
@@ -3,7 +3,17 @@
 import type { ProcessOutput } from 'zx'
 import { $ } from 'zx'
 import { printObject } from './utils'
-const GITHUBURL = 'https://github.com/wAngDna/knowledge-summary.git'
-await $`git remote get-url --all git`.catch((out: ProcessOutput) => {
-  console.log(out)
+const GitHubURL = 'https://github.com/wAngDna/knowledge-summary.git'
+await $`git remote get-url --all github`.catch(async (out: ProcessOutput) => {
+  printObject(out)
+  console.log('remote github now...\n')
+  await $`git remote add github ${GitHubURL}`.catch((out: ProcessOutput) => {
+    printObject(out)
+    throw new Error(out.stdout)
+  })
+})
+
+await $`git push github master`.catch(async (out: ProcessOutput) => {
+  printObject(out)
+  throw new Error(out.stdout)
 })
