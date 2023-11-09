3c99efc5990234230b2bda62a36c3928557cae2b
Dion <Dion@wtex.us>
2023-11-09 16:09:55 +0800

    test: 🚨  test script

--- scripts/check.ts
+++ scripts/check.ts
@@ -4,9 +4,14 @@ import type { ProcessOutput } from 'zx'
 import { $ } from 'zx'
 import { printObject } from './utils'
 
-
 // check stage
 await $`npm run lint:stage`.catch((out: ProcessOutput) => {
+  printObject(out)
+  throw new Error(out.stdout)
+})
+
+// run build
+await $`npm run build`.catch((out: ProcessOutput) => {
   printObject(out)
   throw new Error(out.stdout)
 })
